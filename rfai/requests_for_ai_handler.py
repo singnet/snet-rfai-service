122061780381c3d9849f5ed7e82edea35f25a8c2
prashantramangupta <you@example.com>
2019-10-30 16:29:15 +0530

    addition of API get rfai details for given requester and for given status

--- rfai/requests_for_ai_handler.py
+++ rfai/requests_for_ai_handler.py
@@ -1,10 +1,34 @@
+from common.logger import get_logger
+from common.repository import Repository
+from common.utils import Utils
+from rfai.config import NETWORK_ID, NETWORK, SLACK_HOOK
+from rfai.constant import REQUIRED_KEYS_FOR_GET_RFAI_EVENT
+from rfai.service.rfai_service import RFAIService
+
+rfai = RFAIService(repo=Repository(net_id=NETWORK_ID, NETWORKS=NETWORK))
+util = Utils()
+logger = get_logger(__name__)
+
+
 def request_handler(event, context):
     try:
-        # valid_event = validate_dict(
-        #     data_dict=event, required_keys=REQUIRED_KEYS_FOR_GET_RFAI_EVENT)
-        # if not valid_event:
-        #     return generate_lambda_response(400, "Bad Request", cors_enabled=True)
-        print(event)
-        # response = generate_lambda_response(200, {"status": "success", "data": ""}, cors_enabled=True)
+        logger.info(event)
+        valid_event = util.validate_dict(
+            data_dict=event, required_keys=REQUIRED_KEYS_FOR_GET_RFAI_EVENT)
+        if not valid_event:
+            return util.generate_lambda_response(400, "Bad Request", cors_enabled=True)
+        query_string_parameters = event["queryStringParameters"]
+        response_data = rfai.get_requests(status=query_string_parameters.get("status", None),
+                                          requester=query_string_parameters["requester"])
+        response = util.generate_lambda_response(200, {"status": "success", "data": response_data}, cors_enabled=True)
     except Exception as e:
-        print(repr(e))
+        error_message = util.format_error_message(
+            status="failed",
+            error=repr(e),
+            payload=query_string_parameters,
+            net_id=NETWORK_ID,
+            handler="get-rfai"
+        )
+        util.report_slack(1, error_message, SLACK_HOOK)
+        response = util.generate_lambda_response(500, error_message, cors_enabled=True)
+    return response
---
+++ rfai/service/rfai_service.py
@@ -0,0 +1,13 @@
+from rfai.dao.request_data_access_object import RequestDAO
+from rfai.rfai_status import RFAIStatus
+
+class RFAIService:
+    def __init__(self, repo):
+        self.request_dao = RequestDAO(repo=repo)
+
+    def get_requests(self, status, requester):
+        if status.upper() in RFAIStatus.__members__:
+            request_status = RFAIStatus[status.upper()].value
+        else:
+            raise Exception("Invalid Request status.")
+        return self.request_dao.get_request_data_for_given_requester(status=request_status, requester=requester)
