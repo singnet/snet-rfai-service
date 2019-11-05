a29a1b490e2b7e7a935ac2955416386900c33987
prashantramangupta <you@example.com>
2019-11-05 19:07:11 +0530

    sub status for RFAI request

--- rfai/rfai_status.py
+++ rfai/rfai_status.py
@@ -1,11 +1,25 @@
 from enum import Enum
 
 
+class ApprovedRequestStatus(Enum):
+    ACTIVE = "APPROVED/ACTIVE"
+    SOLUTION_VOTE = "APPROVED/SOLUTION_VOTE"
+    COMPLETED = "APPROVED/COMPLETED"
+    EXPIRED = "APPROVED/EXPIRED"
+
+
+class OpenRequestStatus(Enum):
+    ACTIVE = "OPEN/ACTIVE"
+    EXPIRED = "OPEN/EXPIRED"
+
+
 class RFAIStatus(Enum):
+    OPEN = OpenRequestStatus
+    APPROVED = ApprovedRequestStatus
+
+
+class RFAIStatusCodes(Enum):
     OPEN = 0
     APPROVED = 1
     REJECTED = 2
-    COMPLETED = 3
     CLOSED = 4
-    COMPLETEDFROMUI = 555
-    EXPIRED = 999
